7f796fa6b3f2659f489c846cdaa75002385a00ed
royeradames <royeraadames@gmail.com>
2021-02-11 15:31:18 -0500

    📦access the iframe doc

--- FbAuto.js
+++ FbAuto.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+// const fs = require('fs');
 
 (async () => {
-    
+    //todo: set downlaod to project folder
     const browser = await puppeteer.launch({
         headless:false,
         defaultViewport: null,
@@ -12,23 +12,72 @@ const fs = require('fs');
     });
     const page = await browser.newPage();
     
-    console.log("before loging in")
     await loginToFacebook(page)
     console.log("finishing loging in")
+    
 
-    // save the html page file
-    fs.writeFile('message.html', await page.content(), (err) => {
-        if (err) throw err;
-        console.log('The file has been saved!');
+    // // save the html page file
+    // fs.writeFile('message.html', await page.content(), (err) => {
+    //     if (err) throw err;
+    //     console.log('The file has been saved!');
+    // })
+
+    
+    /*
+        button[disabled=false] does not work reliably on the 1 loadup
+        the document it's loaded by an iframe
+        document.querySelector("iframe").contentWindow.document.querySelector("button")
+
+        notes: 
+        there are 4 child frames total
+        the 1 frame is my target frame. The other 3 frames are it's child frame.
+        id: 16D2888A7EB28013F9D3559662EDD2EE
+        I don't see the contentDocument on it. console.log(await page.mainFrame().childFrames())
+    */
+        // select iframe
+    await page.evaluate(() => {
+        // click the create file
+        const createFile =  document.querySelector("iframe").contentDocument.querySelector("button")
+        createFile.click()
+        debugger
+        //wait for download to be finish
+        // refresh every 1 minute until Pending becomes download
+
+        
+    });
+
+    await page.evaluate(() => {
+        //click available copies
+        const availableCopies =  document.querySelector("iframe").contentDocument.querySelector("li:last-child")
+        availableCopies.click()
+        debugger
+        
     })
+    await page.evaluate(() => {
+        //download data
+        const downloadButton = document.querySelector("iframe").contentDocument.querySelector("button[type=submit]") 
+        downloadButton.click()
+        debugger
+    })
+    await page.evaluate(() => {
 
-    // click the create file
-    // button[disabled=false] does not work reliably on the 1 loadup
-    await page.click("button[disabled=false]")
+        // if ask then re-enter your password
+        console.log("re-enter your password button is found yes or no: ", document.querySelector("input[type=password]") )
 
-    // click available copies
-    // document.querySelector("li:last-child")
-    await page.click("li:last-child")
+        if(document.querySelector("input[type=password]")){
+            debugger
+
+            // element = document.querySelector("input[type=password]")
+            const retypePassword = document.querySelector("input[type=password]")
+            retypePassword.type(process.env.PASS)
+
+            // submit password
+            // document.querySelector("td button[type=submit]")
+            const submitPassword = document.querySelector("td button[type=submit]")
+            submitPassword.click()
+        }
+
+    })
 
     // wait until facebook notifies you that your data is ready.
         /*
@@ -47,20 +96,6 @@ const fs = require('fs');
 
             reload when there is a Pending span and download when there is not 
         */
-       
-
-    // re-enter your password
-        /*
-            element = document.querySelector("input[type=password]")
-
-        */
-
-    // submit password
-        // document.querySelector("td button[type=submit]")
-
-
-    console.log("got data")
-    await page.screenshot({path: '/imgs/finish.png'});
 
     await browser.close();
 
@@ -70,13 +105,16 @@ async function loginToFacebook(page){
     await page.goto('https://www.facebook.com/dyi/?x=AdkadZSUMBkpk0EF&referrer=yfi_settings');
     await page.type("#email", process.env.ID)
     await page.type("#pass",  process.env.PASS)
-    await page.screenshot({path: '/imgs/login.png'});
     await Promise.all([
-        page.waitForSelector("button[disabled=false]"),
+    page.waitForNavigation({ waitUntil: "networkidle0" }),
         page.click('button[type=submit]'),
+
     ]);
 }
 async function facebookLoginCases() {
+    // what happens when the user init get files before login in on our portal?
+    // cancel that download, then go back and start from the beginning 
+    
     if(selector === "#recovery_code_entry"){
         await page.type("#recovery_code_entry", "445364")
         await page.click("button[type=submit]")
