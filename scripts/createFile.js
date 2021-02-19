e3c3a4bf5c15f6e0b5752f452cf98ae06d3b842f
royeradames <royeraadames@gmail.com>
2021-02-19 17:05:52 -0500

    📦move createFile function to it's own file

---
+++ scripts/createFile.js
@@ -0,0 +1,14 @@
+async function createFile(page){
+    
+        
+    // click the create file
+        await Promise.all([
+            page.waitForSelector("[role=heading]"),
+            page.click("button"),
+        ]);
+
+        //todo:what happens when data is already being created?
+        // you can cancel previous and start a new one or click the desable button and act like it's normal.
+    }
+
+module.exports = createFile
