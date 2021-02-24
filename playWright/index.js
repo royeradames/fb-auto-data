3b66f0b9c9f1ac18fc9bfa175cfc8ee9ac3df949
royeradames <royeraadames@gmail.com>
2021-02-24 14:31:58 -0500

    📦init start browser

--- playWright/index.js
+++ playWright/index.js
@@ -1,7 +1,27 @@
 const downloadFile = require("./scripts/downloadFile")
-
+const login = require("./scripts/login")
 index()
 
 async function index() {
-  await downloadFile()
+  /* start browser */
+  const browser = await chromium.launch({ 
+    args: ["--start-maximized", "--disable-notifications",  '--disable-extensions', '--mute-audio'],
+    defaultViewport: null,
+    downloadsPath: "D:\\Lambda\\projects\\puppeteer_test\\data",
+  });
+  const context = await browser.newContext({
+    acceptDownloads: true,
+    viewport: null,
+  })
+  const page = await context.newPage()
+
+  /* save credentials enter by user*/
+  await login()
+  
+  /* ask for files*/
+
+  /* Wait for files*/
+
+  /* Download files*/
+  // await downloadFile()
 }
