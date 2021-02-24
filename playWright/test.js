408944311f045520919a6eac696533d6b298a3c6
royeradames <royeraadames@gmail.com>
2021-02-24 16:19:32 -0500

    🔬testing different ideas

--- playWright/test.js
+++ playWright/test.js
@@ -14,18 +14,33 @@ const { chromium } = require('playwright');
   });;
   await page.goto("https://sample-videos.com/video123/mp4/720/big_buck_bunny_720p_1mb.mp4");
 //   await page.goto("https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg");
+
+    //custom file loader reports
+    // design for downloading 1 complete file at a time 
     let intervalID
     page.on('download', async download => {
-        intervalID = await setInterval( () => { console.log(`File is still downloading. Time ${Date().split(" ")[4]}`)}, 30000)        
+        startDownloadReport(download, intervalID)
+
+        // last report
         download.saveAs(`./data/${ download.suggestedFilename()}`).then(() => {
-            clearInterval(intervalID)
+            endDownloadReport(download, intervalID)
         })
-        console.log(download.suggestedFilename())
-        download.delete()
-        download.createReadStream().then(console.log)
     });
 
     
 //   await browser.close();
 })();
 
+function endDownloadReport(download, intervalID){
+    clearInterval(intervalID)
+    console.log(`Time ${Date().split(" ")[4]}: Finished ${download.suggestedFilename()} file download. `)
+}
+
+  function startDownloadReport(download, intervalID) {
+    // initial file report
+        console.log(`Time ${Date().split(" ")[4]}: Starting ${download.suggestedFilename()} file download. `)  
+    // continues waiting reports
+        intervalID = setInterval( () => { 
+            console.log(`Time ${Date().split(" ")[4]}: File is still downloading. `)
+        }, 30000) 
+  }
