bf5b0db64900a7c81e3cadbbbbb5bc0c3bbf198e
royeradames <royeraadames@gmail.com>
2021-02-24 16:17:54 -0500

    📦convert puppeteer logic for waitiforfile to playwright

---
+++ playWright/scripts/waitForFile.js
@@ -0,0 +1,27 @@
+async function waitForFile(doc){
+    /* select child frame */
+    const frameDocUrl = await (await page.waitForSelector("iframe")).getAttribute("src")
+    const doc = await page.frame({url: frameDocUrl})
+    await doc.waitForLoadState('domcontentloaded');
+
+    /* waitForFile */
+    // refresh every 5 minute until "[role=heading]" is no more 
+    // then Pending becomes download
+    // facebook does not refreshes the page after the content is ready so you have to do it.
+    const fiveMinutes = 300000
+    while(await doc.$("[role=heading]")){
+        console.log(`going to start waiting for 5 min starting in ${Date().split(" ")[4]}`)
+        await doc.waitForTimeout(fiveMinutes)
+        console.log("going to reload")
+        await Promise.All([
+            doc.reload(),
+            doc.waitForLoadState('domcontentloaded'),
+        ])
+        console.log("finish reloading")
+    }
+    await doc.reload();
+    
+    console.log("finish waiting for data")
+}
+
+module.exports = waitForFile
