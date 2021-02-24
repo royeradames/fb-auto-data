3291f877f06ab75c9456272beaf40d704add8b92
royeradames <royeraadames@gmail.com>
2021-02-24 16:18:41 -0500

    👌remove unuse space

--- scripts/createFile.js
+++ scripts/createFile.js
@@ -1,14 +1,12 @@
 async function createFile(page){
-    
-        
     // click the create file
-        await Promise.all([
-            page.waitForSelector("[role=heading]"),
-            page.click("button"),
-        ]);
+    await Promise.all([
+        page.waitForSelector("[role=heading]"),
+        page.click("button"),
+    ]);
 
-        //todo:what happens when data is already being created?
-        // you can cancel previous and start a new one or click the desable button and act like it's normal.
-    }
+    //todo:what happens when data is already being created?
+    // you can cancel previous and start a new one or click the desable button and act like it's normal.
+}
 
 module.exports = createFile
