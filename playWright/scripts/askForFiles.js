4200cd57235e6188c3f497bd29f379fb217eeaab
royeradames <royeraadames@gmail.com>
2021-02-25 14:35:28 -0500

    👌clean up askForFile

--- playWright/scripts/askForFiles.js
+++ playWright/scripts/askForFiles.js
@@ -2,11 +2,13 @@ async function createFile(doc){
     /* create file */
     // select the create file button
     const createFileButton = await doc.$("//button//div[text()='Create File']/../..")
-    // assert if it's able or desable
+    
+    // check if it's able or desable
     // aria-disabled is not the same has general disable attribute
     const buttonDissable =(await createFileButton.getAttribute("aria-disabled"))
     const isButtonEnable = buttonDissable === 'false' ? true : false
-    console.log(isButtonEnable)
+
+    // act different if base on the button avalability 
     if(isButtonEnable){
         //button is enable
         await createFileButton.click()
@@ -14,13 +16,6 @@ async function createFile(doc){
         // button is disable
         console.log("files are already being created")
     }
-    // await Promise.all([
-    //     doc.waitForSelector("[role=heading]"),
-    //     doc.click("button"),
-    // ]);
-    
-    //todo: what to do if the file has been ask already?
-    // you can cancel previous and start a new one or click the desable button and act like it's normal.
 }
 
 module.exports = createFile
