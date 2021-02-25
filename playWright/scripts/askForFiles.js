986dcc6e5c87aff3fd5bd9cdf11d1cf62d0daa9a
royeradames <royeraadames@gmail.com>
2021-02-25 10:22:34 -0500

    👌fix module export

--- playWright/scripts/askForFiles.js
+++ playWright/scripts/askForFiles.js
@@ -9,4 +9,4 @@ async function createFile(page, doc){
     // you can cancel previous and start a new one or click the desable button and act like it's normal.
 }
 
-modules.exports = createFile
+module.exports = createFile
