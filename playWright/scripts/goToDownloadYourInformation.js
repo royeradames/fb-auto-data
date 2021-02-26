33b07b45ba555dc29b3dd240ba7aa9bbaff431ad
royeradames <royeraadames@gmail.com>
2021-02-26 18:45:04 -0500

    📦make doc frame reattach dry

--- playWright/scripts/goToDownloadYourInformation.js
+++ playWright/scripts/goToDownloadYourInformation.js
@@ -1,16 +1,16 @@
+const reattachFrame = require("./reattachFrame")
+
 async function goToDownloadYourInformation(context){
     
     // Create a new page in a pristine context. 
     const page = await context.newPage()
     
     // go to download your information
-    await page.goto("https://www.facebook.com/dyi/?x=AdkadZSUMBkpk0EF&referrer=yfi_settings");
+    await page.goto("https://www.facebook.com/dyi/?x=AdkadZSUMBkpk0EF&referrer=yfi_settings", { waitUntil: 'networkidle'});
     
     //select child frame
-    const frameDocUrl = await (await page.waitForSelector("iframe")).getAttribute("src")
-    const doc = await page.frame({url: frameDocUrl})
-    await doc.waitForLoadState('domcontentloaded');
-
+    const doc = await reattachFrame(page)
+    
     // return the page, and doc
     return [page, doc]
 }
---
+++ playWright/scripts/reattachFrame.js
@@ -0,0 +1,7 @@
+async function reattachFrame (page){
+    const frameDocUrl = await (await page.waitForSelector("iframe")).getAttribute("src")
+    const doc =  await page.frame({url: frameDocUrl})
+    return doc
+}
+
+module.exports = reattachFrame
